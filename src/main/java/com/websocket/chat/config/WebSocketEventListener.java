32f68d2c9c260eca49a90bb7fba4d4940fb73779
HOK Sochetra <chetrafc60@gmail.com>
2024-03-06 12:52:17 +0700

    create UI

--- src/main/java/com/websocket/chat/config/WebSocketEventListener.java
+++ src/main/java/com/websocket/chat/config/WebSocketEventListener.java
@@ -1,10 +1,16 @@
 package com.websocket.chat.config;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
+import com.websocket.chat.chat.ChatMessage;
+import com.websocket.chat.chat.MessageType;
+
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
@@ -12,10 +18,21 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j // for login when user leave the chat
 public class WebSocketEventListener {
     
+    private final SimpMessageSendingOperations messageTemplate;
+
     @EventListener
     public void handleWebSocketDisconnectListener(
         SessionDisconnectEvent event
     ){
-
+        StompHeaderAccessor headerAccessor=StompHeaderAccessor.wrap(event.getMessage());
+        String username=(String) headerAccessor.getSessionAttributes().get("username");
+        if(username !=null){
+            log.info("User disconnected: {}",username);
+            var chatMessage=ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            messageTemplate.convertAndSend("/topic/public",chatMessage);
+        }
     }
 }
